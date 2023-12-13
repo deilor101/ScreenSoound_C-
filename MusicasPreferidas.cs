e5bd66f33b9f80b02bf49d78fbf153456182b166
deilor101 <matehenrique20@hotmail.com>
2023-12-13 18:12:37 -0300

    first commit

---
+++ MusicasPreferidas.cs
@@ -0,0 +1,43 @@
+﻿using ScreenSound_4.Modelos;
+using System.Text.Json;
+
+namespace ScreenSound_4;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+    public List<Musica> ListaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome)
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new List<Musica>();
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        ListaDeMusicasFavoritas.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Essas são as músicas favoritas {Nome}");
+        foreach (var musica in ListaDeMusicasFavoritas)
+        {
+            Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+        }
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json  = JsonSerializer.Serialize(new
+        {
+            nome = Nome, 
+            musicas = ListaDeMusicasFavoritas
+        });
+        string nomeDoArquivo = $"musicas-favoritas-{Nome}.json";
+
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine($"O arquivo JSON foi criado com sucesso! {Path.GetFullPath(nomeDoArquivo)}");
+    }
+ }
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using ScreenSound_4;
+using ScreenSound_4.Filtros;
+using ScreenSound_4.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+	try
+	{
+        string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+		var musicas = JsonSerializer.Deserialize<List<Musica>>(response);
+		LinqFilter.FiltrarMusicasEmCSharp(musicas);
+		//LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+		//LinqOrder.ExibirListaDeArtistasOrdenados(musicas!);
+		//LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+		//LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Tyga");
+		//var musicasPreferidasDoMatheus = new MusicasPreferidas("Matheus");
+		//musicasPreferidasDoMatheus.AdicionarMusicasFavoritas(musicas[1]);
+  //      musicasPreferidasDoMatheus.AdicionarMusicasFavoritas(musicas[78]);
+  //      musicasPreferidasDoMatheus.AdicionarMusicasFavoritas(musicas[899]);
+  //      musicasPreferidasDoMatheus.AdicionarMusicasFavoritas(musicas[356]);
+  //      musicasPreferidasDoMatheus.AdicionarMusicasFavoritas(musicas[775]);
+
+		//musicasPreferidasDoMatheus.ExibirMusicasFavoritas();
+		//musicasPreferidasDoMatheus.GerarArquivoJson();
+    }
+    catch (Exception ex)
+	{
+		Console.WriteLine($"Temos um problema: {ex.Message}"); ;
+	}
+}
